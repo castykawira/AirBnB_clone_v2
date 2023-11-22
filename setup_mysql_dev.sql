4d5d6f3fa09b176c0d17e14cdb8254b884560e0c
castykawira <murithicasty@gmail.com>
2023-11-22 02:42:41 -0800

    question 3

---
+++ setup_mysql_dev.sql
@@ -0,0 +1,16 @@
+-- Create database hbnb_dev_db and user hbnb_dev
+
+-- Create database if not exists
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+
+-- Create user if not exists
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+
+-- Grant privileges to the user
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
+
+-- Grant SELECT privilege on performance_schema
+GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
+
+-- Flush privileges
+FLUSH PRIVILEGES;
