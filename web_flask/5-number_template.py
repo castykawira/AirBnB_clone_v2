0444d3d42ad334b6f6e482541731841c1dd034ff
castykawira <murithicasty@gmail.com>
2023-12-30 06:37:07 -0800

    question 5

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -1,49 +1,47 @@
 #!/usr/bin/python3
-"""
-This script starts a Flask web application with six routes
-"""
-
-from flask import Flask
-
+"""Flask"""
+from flask import Flask, render_template
 app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
-def hello_hbnb():
-    """Display 'Hello HBNB!' when the root path is accessed."""
-    return ('Hello HBNB!')
+def hello():
+    """Return a given string"""
+    return "Hello HBNB!"
 
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
-    """Display 'HBNB' when the /hbnb path is accessed."""
-    return ('HBNB')
+    """Returns a given string"""
+    return "HBNB"
 
 
 @app.route('/c/<text>', strict_slashes=False)
-def c_route(text):
-    """Display 'C ', followed by the value of the text variable."""
-    return 'C {}'.format(text.replace('_', ' '))
+def cText(text):
+    """display C followed by the value of the text variable"""
+    return "C {}".format(text.replace("_", " "))
 
 
-@app.route('/python/', strict_slashes=False)
+@app.route('/python', strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
-def python_route(text='is cool'):
-    """Display 'Python ', followed by the value of the text variable."""
-    return 'Python {}'.format(text.replace('_', ' '))
+def pythonText(text="is cool"):
+    """display Python followed by the value of the text variable"""
+    return "Python {}".format(text.replace("_", " "))
 
 
 @app.route('/number/<int:n>', strict_slashes=False)
-def number_route(n):
-    """Display 'n is a number' only if n is an integer."""
-    return '{} is a number'.format(n)
+def isNumber(n):
+    """display “n is a number” only if n is an integer"""
+    if isinstance(n, int):
+        return "{} is a number".format(n)
 
 
 @app.route('/number_template/<int:n>', strict_slashes=False)
-def number_template(n):
-    """Display an HTML page with 'Number: n' inside the H1 tag."""
-    return render_template('5-number.html', number=n)
+def number_template(n=None):
+    """display a HTML page only if n is an integer"""
+    if isinstance(n, int):
+        return render_template("5-number.html", n=n)
 
 
 if __name__ == "__main__":
-    app.run(port='5000', host='0.0.0.0')
+    app.run()
